caa00a47f302241023abb1d552d3313a112a355c
Nadège Thierry <nadege@lacouleurduzebre.com>
2026-09-17 15:48:56 +0200

    Compatibilité EasyAdmin 5

--- src/Admin/Field/EasyMediaField.php
+++ src/Admin/Field/EasyMediaField.php
@@ -7,12 +7,13 @@ namespace Adeliom\EasyMediaBundle\Admin\Field;
 use Adeliom\EasyMediaBundle\Form\EasyMediaType;
 use EasyCorp\Bundle\EasyAdminBundle\Contracts\Field\FieldInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Field\FieldTrait;
+use Symfony\Contracts\Translation\TranslatableInterface;
 
 class EasyMediaField implements FieldInterface
 {
     use FieldTrait;
 
-    public static function new(string $propertyName, ?string $label = null): self
+    public static function new(string $propertyName, TranslatableInterface|string|bool|null $label = null): self
     {
         return (new self())
             ->setProperty($propertyName)
